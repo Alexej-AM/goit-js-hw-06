06232ac5c2d72f9c956cb411e6b7cc4ddf4a6c9a
Alexej Melnyk <melnyk.alexej@gmail.com>
2022-07-02 22:04:00 +0300

    6+

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,12 @@
 const inputEl = document.querySelector('#validation-input');
 console.log(inputEl);
-const lengthSymInInputEl = +inputEl.dataset.length;
+const lengthSymInInputEl = inputEl.dataset.length;
 
 
 inputEl.addEventListener('blur', onLengthInInput);
 
 function onLengthInInput(event) { 
-    if (event.currentTarget.value.length === lengthSymInInputEl) { 
+    if (event.currentTarget.value.length === Number(lengthSymInInputEl)) { 
         inputEl.classList.toggle('valid');
     //    
     }
