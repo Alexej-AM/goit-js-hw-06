89dee1271529c9e9ea307581058508101ed7878b
Alexej Melnyk <melnyk.alexej@gmail.com>
2022-07-02 16:29:44 +0300

    1 +

---
+++ js/task-01.js
@@ -0,0 +1,23 @@
+
+
+const categoriesRef = document.querySelector('#categories');
+console.log('Number of categories:', categoriesRef.children.length);
+
+const titleRef = document.querySelectorAll('h2');
+
+const elemenstRef = document.querySelectorAll('.item ul');
+
+
+
+const firstEl =
+    console.log('Category:', titleRef[0].textContent);
+    console.log('Elements:', elemenstRef[0].children.length);
+
+const secondEl = 
+console.log('Category:', titleRef[1].textContent);
+    console.log('Elements:', elemenstRef[1].children.length);
+
+
+const lastEl = 
+    console.log('Category:', titleRef[2].textContent);
+    console.log('Elements:', elemenstRef[2].children.length);
