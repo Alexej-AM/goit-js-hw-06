2b5d2f423649bb9bf9d8231ade74c526eef1ad01
Alexej Melnyk <melnyk.alexej@gmail.com>
2022-07-03 11:58:29 +0300

    8+

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,30 @@
+const form = document.querySelector('.login-form');
 
+
+form.addEventListener('submit', onFormSubmit);
+
+
+function onFormSubmit(event) { 
+    event.preventDefault();
+      const formElements = event.currentTarget.elements;
+
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+
+    if (mail === "" || password === "") {
+        alert('Поле не может быть пустым !!!');
+        
+    }
+
+  
+
+    const formData = {
+        mail,
+        password,
+    }
+
+    event.currentTarget.reset();
+    
+    console.log(formData);
+
+};
