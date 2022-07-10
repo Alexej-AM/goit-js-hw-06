69b9ae3b5ffd08bafd7e76555973cdb7744e46fb
Alexej Melnyk <melnyk.alexej@gmail.com>
2022-07-10 23:11:58 +0300

    +1

--- js/task-01.js
+++ js/task-01.js
@@ -3,21 +3,35 @@
 const categoriesRef = document.querySelector('#categories');
 console.log('Number of categories:', categoriesRef.children.length);
 
-const titleRef = document.querySelectorAll('h2');
-
-const elemenstRef = document.querySelectorAll('.item ul');
+const itemRef = document.querySelectorAll('.item');
 
 
 
-const firstEl =
-    console.log('Category:', titleRef[0].textContent);
-    console.log('Elements:', elemenstRef[0].children.length);
-
-const secondEl = 
-console.log('Category:', titleRef[1].textContent);
-    console.log('Elements:', elemenstRef[1].children.length);
 
 
-const lastEl = 
-    console.log('Category:', titleRef[2].textContent);
-    console.log('Elements:', elemenstRef[2].children.length);
+const qqq = itemRef.forEach((element) => {
+    console.log("Category: ", element.firstElementChild.textContent);
+    console.log("Elements: ", element.lastElementChild.children.length);
+});
+
+
+
+
+
+
+
+
+
+
+// const firstEl =
+//     console.log('Category:', titleRef[0].textContent);
+//     console.log('Elements:', elemenstRef[0].children.length);
+
+// const secondEl = 
+// console.log('Category:', titleRef[1].textContent);
+//     console.log('Elements:', elemenstRef[1].children.length);
+
+
+// const lastEl = 
+//     console.log('Category:', titleRef[2].textContent);
+//     console.log('Elements:', elemenstRef[2].children.length);
