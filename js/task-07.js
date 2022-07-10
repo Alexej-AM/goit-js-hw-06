b55872fb0cb2b90958652119bdc2381ca9fbdaf2
Alexej Melnyk <melnyk.alexej@gmail.com>
2022-07-10 22:09:34 +0300

    7+

--- js/task-07.js
+++ js/task-07.js
@@ -8,6 +8,8 @@ const fontSizeText = document.querySelector('#text');
 sizeFontControl.addEventListener('input', onChangeFontSize);
 
 
+fontSizeText.style.fontSize = sizeFontControl.value + 'px'
+
 function onChangeFontSize(event) { 
         console.log(fontSizeText.style.fontSize = sizeFontControl.value + 'px');
 };
